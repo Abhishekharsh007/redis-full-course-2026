4d739c27c6cbbd8d1bf3e6149afc36f7432975ff
Abhishekharsh007 <abhishekharsh3411@gmail.com>
2026-08-18 12:44:38 +0530

    feat: add Redis client connection and disconnection functionality

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import app from "./app";
 import { testConnection } from "./db/pool";
+import { connectRedis, disconnectRedis } from "./redis/client";
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 5000;
 async function startServer() {
   try {
     await testConnection();
+    await connectRedis();
 
     app.listen(PORT, () => {
       console.log(`Server running on http://localhost:${PORT}`);
@@ -18,5 +20,10 @@ async function startServer() {
     process.exit(1);
   }
 }
+
+process.on("SIGINT", async () => {
+  await disconnectRedis();
+  process.exit(0);
+});
 
 startServer();
